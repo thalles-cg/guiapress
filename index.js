babed52ad18f8ef7a2e42ddada9834a325c918fb
thalles-cg <thalles.pity@gmail.com>
2025-08-22 11:11:18 -0300

    feat: Criação de rotas e controllers para articles e categories

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const app = express();
 const bodyParser = require('body-parser');
 const connection = require('./database/database');
 
+const categoriesController = require('./categories/CategoriesController')
+const articlesController = require('./articles/ArticlesController')
+
 app.set('view engine', 'ejs');
 
 app.use(express.static('public'));
@@ -18,6 +21,9 @@ connection
    .catch((error) => {
       console.log(error);
    });
+
+app.use('/',categoriesController)
+app.use('/',articlesController)
 
 app.get('/', (req, res) => {
    res.render('index');
